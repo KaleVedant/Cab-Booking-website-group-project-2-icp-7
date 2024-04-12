195f199190eab7da4080b1efc84b5a871d00e0de
KaleVedant <vedant35kale@gmail.com>
2024-04-12 22:55:15 +0530

    new

---
+++ javascript/about.js
@@ -0,0 +1,27 @@
+
+function lightmode(){
+    const imgdark = document.getElementById("btn-light");
+    imgdark.classList.add('active');
+
+    const imglight = document.getElementById("btn-dark");
+    imglight.classList.remove('active');
+
+    const  bodyElement = document.getElementById("body");
+    bodyElement.className = "theme-light";
+
+    co
+}
+
+function darkmode(){
+    const imgdark = document.getElementById("btn-light");
+    imgdark.classList.remove('active');
+
+    const imglight = document.getElementById("btn-dark");
+    imglight.classList.add('active');
+
+    const  bodyElement = document.getElementById("body");
+    bodyElement.className = "theme-dark";
+    
+
+
+}
